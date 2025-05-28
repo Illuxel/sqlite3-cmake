8d0d0daed4c984fae9fdbd0e50d52a4f3c8a1af9
Illia Samarin <illia.samarin.ua@gmail.com>
2025-05-28 12:59:11 +0300

    Updated sqlite3 to 3.49.2

--- cmake/sqlite3-config.cmake
+++ cmake/sqlite3-config.cmake
@@ -1,6 +1,2 @@
 @PACKAGE_INIT@
-
-include(${CMAKE_CURRENT_LIST_DIR}/sqlite3-targets.cmake)
-include(CMakeFindDependencyMacro)
-
-find_dependency(OpenSSL)
+include( ${CMAKE_CURRENT_LIST_DIR}/sqlite3-targets.cmake )
